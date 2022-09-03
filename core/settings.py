0f67f865792193c3501baf1badb2d91f523687c5
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-09-03 19:53:04 +0100

    chore: configure the `allowed hosts` list to futher deploy

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '31^+j4)+%$h2u1ywsduq0cwgmt#+fxrp1d^ykyn9utp+@ry=ud'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
