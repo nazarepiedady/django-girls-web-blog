78b8c7f213acce027fa7510d5f597550024744c6
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-09-06 12:18:51 +0100

    chore: do a relationship between author of post and `settings.AUTH_USER_MODEL`

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
